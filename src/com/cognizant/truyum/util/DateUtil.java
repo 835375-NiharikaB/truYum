5692d1f2077c7d9dc71266badf0ea9c99819ba6b
Bejugam <835375@cognizant.com>
2020-01-02 19:11:53 +0530

    Java

--- src/com/cognizant/truyum/util/DateUtil.java
+++ src/com/cognizant/truyum/util/DateUtil.java
@@ -1,5 +1,20 @@
 package com.cognizant.truyum.util;
 
-public class DateUtil {
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+public class DateUtil {
+    public static Date convertToDate(String date) {
+        try {
+            SimpleDateFormat format = new SimpleDateFormat("dd/mm/yyyy");
+            format.setLenient(false);
+            return format.parse(date);// String to Date
+
+        } catch (ParseException e) {
+            System.out.println("Date/format went wrong");
+        }
+        return null;// never executed
+    }
 }
+
